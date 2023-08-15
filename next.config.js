5bfccf8d5ad28cabdb224e5e22f76e12e453208c
Cip <cip@macbook-pro-m1-16.local>
2023-08-15 08:03:18 +0400

    first one

--- next.config.js
+++ next.config.js
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    async redirects() {
+        return [
+            {
+                source: '/page-1',
+                destination: '/page-1-redirected',
+                permanent: true,
+            },
+            {
+                source: '/',
+                destination: '/page-redirected',
+                permanent: true,
+            },
+        ]
+    },
+}
 
 module.exports = nextConfig
